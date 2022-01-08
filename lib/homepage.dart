c7920ffd0cd0d29dc2e27dc794339ffd26b36442
hansalkothari <hansalkothari3@gmail.com>
2022-01-08 21:56:48 +0530

    icons created , clock aligned , page almost done

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:alarm_clock/clockview.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,11 +12,100 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE,d MMM').format(now);
+    var timezoneString = now.timeZoneOffset.toString().split(".").first;
+    var offsetSign = '';
+    if (!timezoneString.startsWith('-')) offsetSign = '-';
+    print(timezoneString);
+
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        color: Color(0xFF2D2F41),
-        child: ClockView(),
+      backgroundColor: Color(0xFF2D2F41),
+      body: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildMenuButton('Clock', 'assets/clock_icon.png'),
+              buildMenuButton('Alarm', 'assets/alarm_icon.png'),
+              buildMenuButton('Timer', 'assets/timer_icon.png'),
+              buildMenuButton('StopWatch', 'assets/stopwatch_icon.png'),
+            ],
+          ),
+          VerticalDivider(
+            color: Colors.white38,
+            width: 1,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(32),
+              alignment: Alignment.center,
+              color: Color(0xFF2D2F41),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Clock',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    formattedTime,
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    formattedDate,
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  ClockView(),
+                  Text(
+                    'Timezone',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.language, color: Colors.white),
+                      SizedBox(width: 16),
+                      Text(
+                        'UTC' + offsetSign + timezoneString,
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding buildMenuButton(String title, String image) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: FlatButton(
+        onPressed: () {},
+        child: Column(
+          children: [
+            Image.asset(
+              image,
+              scale: 1.5,
+            ),
+            SizedBox(height: 16),
+            Text(
+              title ?? ' ',
+              style: TextStyle(
+                  fontFamily: 'avenir', color: Colors.white38, fontSize: 14),
+            )
+          ],
+        ),
       ),
     );
   }
